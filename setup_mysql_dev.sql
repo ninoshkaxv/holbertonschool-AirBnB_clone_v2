857370e3d840fcd0c8e7cfac9b6e8cf9c15cee15
ninoshkaxv <perez.ninoshka31@gmail.com>
2024-04-12 21:32:38 -0400

    fix error

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,16 +1,6 @@
--- Prepare MySQL server
--- Create database
+-- Prepares the MySQL server for this project
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
-
--- Create user
 CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
-
--- Grant privileges
-GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
-
--- Grant SELECT privileges
-GRANT
-SELECT
-    ON performance_schema.* TO 'hbnb_dev'@'localhost';
-
+GRANT ALL PRIVILEGES ON `hbnb_dev_db`.* TO 'hbnb_dev'@'localhost';
+GRANT SELECT ON `performance_schema`.* TO 'hbnb_dev'@'localhost';
 FLUSH PRIVILEGES;
